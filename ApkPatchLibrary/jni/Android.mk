bc93559d95a7a1e1ada88e7a9b7d209c2bb7b6d7
yanping <ypist@qq.com>
2016-07-14 01:49:10 +0800

    1.编译 ApkPatchLibrary 各个版本(armeabi, armeabi-v7a,x86); 在开始编译时，会报 error: undefined reference to 'err' 和 error: undefined reference to 'errx'; 经过查找,是因为我的系统库没有这两个函数,于是直接替换成printf编译; ( http://linux.die.net/man/3/errx ) err: Display the current errno information string and exit; errx: Display an error message and exit; 所以，我们可以把对应到的地方替换成 printf 和 exit 两个函数组合;

---
+++ ApkPatchLibrary/jni/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := ApkPatchLibrary
+LOCAL_SRC_FILES  := com_cundong_utils_PatchUtils.c
+
+LOCAL_LDLIBS     := -lz -llog
+
+include $(BUILD_SHARED_LIBRARY)
+
+APP_ABI := armeabi armeabi-v7a x86
